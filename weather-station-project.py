b3c2186d4146eaf365d9fe7f261f5a5dd88817d9
John Moczygemba <johnm250r@gmail.com>
2020-06-14 20:36:01 -0400

    changed to keyboard interrupt

--- weather-station-project.py
+++ weather-station-project.py
@@ -2,18 +2,12 @@ import serial
 from time import time
 
 ser = serial.Serial('COM4', baudrate = 9600, timeout=1)
-
-#for closing the port, port must be closed after each run or the next run wont have permission
-#ser.close()
-
-end = time() + 60
-
-while time() < end:
-    arduinoData = ser.readline()
-    print(arduinoData)
-
-ser.close()
-
-'''while 1:
-    arduinoData = ser.readline()
-    print(arduinoData)'''
+    
+while True:
+    try:
+        arduinoData = ser.readline()
+        print(arduinoData)
+    except: #Ctrl-C to interrupt
+        ser.close()
+        print("Keyboard interrupt")
+        break
