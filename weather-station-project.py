18016eef101a9da838bfa7c0984cd5eaf09ea918
John Moczygemba <johnm250r@gmail.com>
2020-06-23 23:47:31 -0400

    added realtime

--- weather-station-project.py
+++ weather-station-project.py
@@ -1,5 +1,8 @@
 import serial
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import datetime
 
 ser = serial.Serial('COM4', baudrate = 9600, timeout=1)
 #ser.reset_input_buffer() #might be needed
@@ -10,21 +13,24 @@ while True:
         dataStr = arduinoData.decode("utf-8")
         strippedString = dataStr.rstrip()
         dataList = strippedString.split(",")
+        
         if len(dataList) == 6: #add an else statement to print what data i did get
-            humidity = dataList[0]
+            '''humidity = dataList[0]
             temperature1 = dataList[1]
             pressure = dataList[2]
             temperature2 = dataList[3]
-            light = dataList[4]
-            '''print(humidity)
-            print(temperature1)
-            print(pressure)
-            print(temperature2)
-            print(light)
-            print('\n')'''
+            light = dataList[4]'''
+            
             dataArray_str = np.array(dataList)
             dataArray = dataArray_str.astype(float)
+            
+            currentTime = datetime.datetime.now()
+            #time = matplotlib.dates.date2num(currentTime) #can we even use this float generated here?
+            
+            #plt.plot(time, dataArray[0])
+            
             print(dataArray)
+            print("%s:%s:%s" % (currentTime.hour, currentTime.minute, currentTime.second))
         
     except: #Ctrl-C to interrupt
         ser.close()
