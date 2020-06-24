6232c71365fc738a9b84568d16c4d20dd6ec9d2d
John Moczygemba <johnm250r@gmail.com>
2020-06-23 23:53:02 -0400

    cleaned up comments

--- weather-station-project.py
+++ weather-station-project.py
@@ -1,7 +1,5 @@
 import serial
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 import datetime
 
 ser = serial.Serial('COM4', baudrate = 9600, timeout=1)
@@ -15,19 +13,11 @@ while True:
         dataList = strippedString.split(",")
         
         if len(dataList) == 6: #add an else statement to print what data i did get
-            '''humidity = dataList[0]
-            temperature1 = dataList[1]
-            pressure = dataList[2]
-            temperature2 = dataList[3]
-            light = dataList[4]'''
             
             dataArray_str = np.array(dataList)
             dataArray = dataArray_str.astype(float)
             
             currentTime = datetime.datetime.now()
-            #time = matplotlib.dates.date2num(currentTime) #can we even use this float generated here?
-            
-            #plt.plot(time, dataArray[0])
             
             print(dataArray)
             print("%s:%s:%s" % (currentTime.hour, currentTime.minute, currentTime.second))
