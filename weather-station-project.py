91684cbe46cda92820779d4cd1500a39333af156
John Moczygemba <johnm250r@gmail.com>
2020-06-23 21:27:53 -0400

    data is put into an array

--- weather-station-project.py
+++ weather-station-project.py
@@ -16,51 +16,17 @@ while True:
             pressure = dataList[2]
             temperature2 = dataList[3]
             light = dataList[4]
-            print(humidity)
+            '''print(humidity)
             print(temperature1)
             print(pressure)
             print(temperature2)
             print(light)
-            print('\n')
-            
-        #dataArray_str = np.array(dataList)
-        #dataArray = np.fromstring(dataArray_str, dtype = np.float, sep = ', ')
-        #dataArray = dataArray_str.astype(float)
+            print('\n')'''
+            dataArray_str = np.array(dataList)
+            dataArray = dataArray_str.astype(float)
+            print(dataArray)
         
-        
-        #humidity = dataArray_str[0]
-        #temperature1 = dataArray_str[1]
-        #pressure = dataArray_str[2]
-        #temperature2 = dataArray_str[3]
-        #light = dataArray_str[4]
-        #voltage = dataArray_str[5]
-        
-        #print(humidity)
-        #print(temperature1)
-        #print(pressure)
-        #print(temperature2)
-        #print(light)
-       # print(dataArray_str)
     except: #Ctrl-C to interrupt
         ser.close()
         print("Keyboard interrupt")
         break
-'''while True:
-    arduinoData = ser.readline()
-    dataStr = arduinoData.decode("utf-8")
-    strippedString = dataStr.rstrip()
-    dataList = strippedString.split(",")
-    if len(dataList) == 6:
-        humidity = dataList[0]
-        temperature1 = dataList[1]
-        print(humidity)
-        print(temperature1)
-    #dataArray_str = np.array(dataList)
-        #dataArray = np.fromstring(dataArray_str, dtype = np.float, sep = ', ')
-    #print(dataList)
-    #humidity = dataList[0]
-    #temperature1 = dataList[1]
-    #print(humidity)
-    #print(temperature1)
-
-    #print(dataArray_str)'''
