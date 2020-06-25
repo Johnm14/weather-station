c3216cfe42cee961be08c953fb326a95746c8a72
John Moczygemba <johnm250r@gmail.com>
2020-06-24 21:00:09 -0400

    exports data to .csv

--- weather-station-project.py
+++ weather-station-project.py
@@ -1,9 +1,11 @@
+#Todo
+#Reads directory for a file of a certain name to make sure it doesnt log on an existing file
 import serial
 import numpy as np
 import datetime
+import csv
 
 ser = serial.Serial('COM4', baudrate = 9600, timeout=1)
-#ser.reset_input_buffer() #might be needed
     
 while True:
     try:
@@ -12,7 +14,7 @@ while True:
         strippedString = dataStr.rstrip()
         dataList = strippedString.split(",")
         
-        if len(dataList) == 6: #add an else statement to print what data i did get
+        if len(dataList) == 6: #add an else statement to print what data i did get?
             
             dataArray_str = np.array(dataList)
             dataArray = dataArray_str.astype(float)
@@ -21,6 +23,14 @@ while True:
             
             print(dataArray)
             print("%s:%s:%s" % (currentTime.hour, currentTime.minute, currentTime.second))
+            
+            with open("%s_%s.csv" % (currentTime.month, currentTime.day), "a", newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=",")
+                
+                writer.writerow(["%s:%s:%s" % (currentTime.hour, currentTime.minute,
+                                                  currentTime.second), dataArray[0], dataArray[1],
+                                                  dataArray[2], dataArray[3],
+                                                  dataArray[4], dataArray[5]])
         
     except: #Ctrl-C to interrupt
         ser.close()
